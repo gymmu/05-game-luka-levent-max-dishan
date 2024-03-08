611454d9ac5f1ddebd5afb29ad6b1b99fa042281
Max Favor <e282234@edu.sbl.ch>
2024-03-08 12:15:09 +0100

    Game fixed. Dijan and levents fault

--- src/scenes/level-02.js
+++ src/scenes/level-02.js
@@ -17,26 +17,27 @@ k.scene("level-02", async () => {
   await generateMapRPG("maps/level-02.txt")
 
   addGeneralGameLogic()
+})
+k.onCollide("player", "cave", (player, cave) => {
+  if (cave.door === 1) {
+    k.go("level-02")
+  }
+  if (player.hasCave === true) {
+    k.go("finish")
+  }
+})
 
+k.onCollide("player", "cave", (player) => {
+  if (player.hasFlower === true) {
+  }
+  k.go("level-03")
+})
 
-  k.onCollide("player", "cave", (player, cave) => {
-    if (cave.door === 1) {
-      k.go("level-02")
-    }
-    if (player.hasCave === true) {
-      k.go("finish")
+k.onCollide("player", "flower", (player, flower) => {
+  flower.destroy()
+  player.hasFlower = true
+})
 
-  k.onCollide("player", "cave", (player) => {
-    if (player.hasFlower === true) {
-
-    }
-    k.go("level-03")
-  })
-
-  k.onCollide("player", "flower", (player, flower) => {
-    flower.destroy()
-    player.hasFlower = true
-  })
 k.onUpdate(() => {
   const player = k.get("player")[0]
   if (player.pos.y > 720) {
