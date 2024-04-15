374c7e4a2713a67507979a0a0bf97d9d4875bc46
Levent Bal <e227405@edu.sbl.ch>
2024-04-15 08:41:27 +0200

    fixed now wirklich the background

--- src/sprites.js
+++ src/sprites.js
@@ -298,8 +298,8 @@ export default function loadSprites() {
   })
   k.loadSpriteAtlas("sprites/silcProjectile.png", {
     silcRight: { x: 0 * TS, y: 1 * TS, width: TS, height: TS },
-    k.loadSpriteAtlas("sprites/battelground1.png", {
-      background: { x: 0, y: 0, width: 1920, height: 1080 }, // Hier geben wir in Pixeln an, wo die Grafik für das Spielobjekt anfängt, und aufhört (oben-links bis unten-rechts).
-    })
+  })
+  k.loadSpriteAtlas("sprites/Battelground1.png", {
+    background: { x: 0, y: 0, width: 1920, height: 1080 }, // Hier geben wir in Pixeln an, wo die Grafik für das Spielobjekt anfängt, und aufhört (oben-links bis unten-rechts).
   })
 }
